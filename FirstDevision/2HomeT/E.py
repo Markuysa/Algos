28ad1d89a4c50b869ce39278906234b999ce70a4
Markuysa <islam.kus0101@gmail.com>
2023-02-20 16:16:25 +0300

    fixed_E

--- FirstDevision/2HomeT/E.py
+++ FirstDevision/2HomeT/E.py
@@ -21,33 +21,3 @@ if place != 0:
     resPlace += 1
 print(resPlace)
 
-
-def sol(a, n):
-    max_a = max(a)
-    t_dist = 0  # столько точно не выбил
-    to_find = False
-
-    for i in range(n - 1):
-        if a[i] == max_a and not to_find:
-            to_find = True
-        elif to_find:
-            a2 = a[i]
-            a3 = a[i + 1]
-            if (a2 % 10) == 5 and a2 > a3:
-                if a2 > t_dist:
-                    t_dist = a2
-    print(t_dist)
-    if t_dist:  # найден в списке
-        t_pos = 1
-        for dist in a:
-            if dist > t_dist:
-                t_pos += 1
-        return t_pos
-    else:
-        return 0
-
-
-n = int(input())
-a = list(map(int, input().split()))
-
-print(sol(a, n))
